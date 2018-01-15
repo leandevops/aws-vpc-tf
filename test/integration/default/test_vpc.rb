781b0370155576590ce072ffc6aa7c92d834b778
lestex <lestex@gmail.com>
2018-01-15 12:46:36 +0300

    updates tests for VPC

--- test/integration/default/test_vpc.rb
+++ test/integration/default/test_vpc.rb
@@ -9,15 +9,19 @@ tf_state = JSON.parse(File.open(tf_state_file).read)
 
 # testing vars from .tfvars file
 target_vpc_name = terraform_tfvars_config['name']
+target_vpc_env = terraform_tfvars_config['environment']
 target_cidr_block = terraform_tfvars_config['vpc_cidr']
 target_vpc_region = terraform_tfvars_config['region']
 
 vpc_id = tf_state['modules'][1]['outputs']['vpc_id']['value']
 
+# testing VPC
 describe vpc(vpc_id) do
   it { should exist }
   it { should be_available }
 
-  it { should have_tag('Name').value(target_vpc_name) }
+	it { should have_tag('Name').value(target_vpc_name) }
+	it { should have_tag('environment').value(target_vpc_env) }
+	it { should have_tag('builtWith').value('terraform') }
 	its(:cidr_block) { should eq target_cidr_block }	
 end
